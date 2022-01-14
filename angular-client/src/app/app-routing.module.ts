e335cfcbb5b4cafc5c3bbe0f17efb554873144d0
Arapsih Güngör <a.guengoer@austrian-standards.at>
2022-01-14 16:14:11 +0100

    add deparment und user list

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DepartmentListComponent} from "./department-list/department-list.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {UserListComponent} from "./user-list/user-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'department-list', component: DepartmentListComponent},
+  {path: 'user-list', component: UserListComponent},
+  {path: '', redirectTo: '/department-list', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentListComponent,
+    PageNotFoundComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
